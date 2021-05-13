4ec03d19b547cfa39b5185758f0ef8663eefaa6f
skyandy1891 <skyandy1891@gmail.com>
2021-05-13 23:07:58 +0300

    task 18 added

--- src/module-1.js
+++ src/module-1.js
@@ -8,3 +8,30 @@ function checkStorage(available, ordered) {
   }
   return message;
 }
+// ============ 17 ==========
+
+let a = 5;
+let b = 10;
+let c = 15;
+let d = 20;
+
+// Change code below this line
+a += a = 2;
+b -= b = 4;
+c *= c = 3;
+d /= d = 10;
+
+// ============ 18 ==========
+
+function makeTransaction(pricePerDroid, orderedQuantity, customerCredits) {
+  let message;
+  // Change code below this line
+  const totalPrice = pricePerDroid * orderedQuantity;
+  if (totalPrice > customerCredits) {
+    message = `Insufficient funds!`;
+  } else {
+    message = `You ordered ${orderedQuantity} droids, you have ${(customerCredits -=
+      totalPrice)} credits left`;
+  }
+  return message;
+}
