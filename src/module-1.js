279c4a10a3492381580df5bb44afa102a6376fbf
skyandy1891 <skyandy1891@gmail.com>
2021-05-13 23:13:03 +0300

    task 19

--- src/module-1.js
+++ src/module-1.js
@@ -35,3 +35,22 @@ function makeTransaction(pricePerDroid, orderedQuantity, customerCredits) {
   }
   return message;
 }
+
+// ============ 19 ==========
+
+function checkPassword(password) {
+  const ADMIN_PASSWORD = 'jqueryismyjam';
+  let message;
+
+  if (password === null) {
+    // Change this line
+    message = 'Canceled by user!';
+  } else if (password === 'jqueryismyjam') {
+    // Change this line
+    message = 'Welcome!';
+  } else {
+    message = 'Access denied, wrong password!';
+  }
+
+  return message;
+}
