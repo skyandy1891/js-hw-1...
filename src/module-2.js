decff6df3a00de317a41f8cd0cd10e496cbc812f
skyandy1891 <skyandy1891@gmail.com>
2021-05-17 17:56:48 +0300

    task 8 added

--- src/module-2.js
+++ src/module-2.js
@@ -1,21 +1,63 @@
 // ========== 1 ==========
-В функции может быть больше одного оператора return.Главное помнить, что выполнение функции прерывается когда интерпретатор встречает возврат, и весь код после него будет проигнорирован в текущем вызове функции.
 
-Возьмём уже знакомую нам функцию проверки совершеннолетия. Она работает, но здесь есть «лишний» код, то есть тело функции можно оптимизировать. В данном случае подойдёт приём (паттерн) «ранний возврат».
-
-
-Если условие в if выполняется, то есть приводится к true, возвращаем строку "You are an adult" и код ниже уже не исполнится.
-Если условие в if не выполняется, то есть приводится к false, возвращаем строку "You are a minor".
-Используя паттерн «ранний возврат» и то, что выполнение функции прерывается на операторе return, мы избавляемся от лишней переменной и блока else. То есть этот приём помогает «разгладить» ветвления.
-
-Задание
-Запиши условие в инструкции if так, чтобы функция работала правильно.
-// решение
 function checkAge(age) {
-  if (age >= 18) { 
-    return "You are an adult";
+  if (age >= 18) {
+    return 'You are an adult';
   }
 
-  return "You are a minor";
+  return 'You are a minor';
 }
 // ========== 2 ==========
+
+function checkPassword(password) {
+  const ADMIN_PASSWORD = 'jqueryismyjam';
+
+  if (password === ADMIN_PASSWORD) {
+    return 'Welcome!';
+  }
+  return 'Access denied, wrong password!';
+}
+
+// ========== 3 ==========
+
+function checkStorage(available, ordered) {
+  if (ordered === 0) {
+    return 'Your order is empty!';
+  }
+  if (ordered > available) {
+    return 'Your order is too large, not enough goods in stock!';
+  }
+  return 'The order is accepted, our manager will contact you';
+}
+
+// ========== 4 ==========
+
+// Объяви переменную fruits и присвой ей массив фруктов - строк "apple", "plum", "pear" и "orange".
+const fruits = ['apple', 'plum', 'pear', 'orange'];
+
+// ========== 5 ==========
+
+const fruits = ['apple', 'plum', 'pear', 'orange'];
+const firstElement = fruits[0];
+const secondElement = fruits[1];
+const lastElement = fruits[3];
+
+// ========== 6 ==========
+
+const fruits = ['apple', 'plum', 'pear', 'orange'];
+
+fruits[1] = `peach`;
+fruits[3] = `banana`;
+
+// ========== 7 ==========
+//  длина массива
+const fruits = ['apple', 'peach', 'pear', 'banana'];
+const fruitsArrayLength = fruits.length;
+
+// ========== 8 ==========
+
+const fruits = ['apple', 'peach', 'pear', 'banana'];
+const lastElementIndex = fruits.length - 1;
+const lastElement = fruits[lastElementIndex];
+
+// ========== 9 ==========
