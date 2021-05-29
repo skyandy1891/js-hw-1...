22b0a8ed824b16134da795253edcaac138afa498
skyandy1891 <skyandy1891@gmail.com>
2021-05-29 21:30:18 +0300

    task 26 added

--- src/module-3.js
+++ src/module-3.js
@@ -137,3 +137,273 @@ const credentials = {
 };
 
 // =============== 10 ===============
+
+const apartment = {
+  descr: 'Spacious apartment in the city centre',
+  rating: 4,
+  price: 2153,
+};
+const keys = [];
+const values = [];
+for (key in apartment) {
+  keys.push(key);
+  values.push(apartment[key]);
+}
+
+// =============== 11 ===============!!!!!
+
+const keys = [];
+const values = [];
+const advert = {
+  service: 'apt',
+};
+const apartment = Object.create(advert);
+apartment.descr = 'Spacious apartment in the city center';
+apartment.rating = 4;
+apartment.price = 2153;
+
+for (const key in apartment) {
+  if (apartment.hasOwnProperty(key)) {
+    keys.push(key);
+    values.push(apartment[key]);
+  }
+}
+
+// =============== 12 ===============!!!!!
+
+function countProps(object) {
+  let propCount = 0;
+  for (const obj in object) {
+    if (object.hasOwnProperty(obj)) {
+      propCount += 1;
+    }
+  }
+  return propCount;
+}
+
+// =============== 13 ===============!!!!!
+
+const apartment = {
+  descr: 'Spacious apartment in the city center',
+  rating: 4,
+  price: 2153,
+};
+const values = [];
+const keys = Object.keys(apartment);
+for (const key of keys) {
+  values.push(apartment[key]);
+}
+
+// =============== 14 ===============!!!!!
+
+function countProps(object) {
+  let propCount = 0;
+
+  const keys = Object.keys(object);
+
+  for (const key of keys) {
+    propCount += 1;
+  }
+  return propCount;
+}
+
+// =============== 15 ===============!!!!!
+
+const apartment = {
+  descr: 'Spacious apartment in the city center',
+  rating: 4,
+  price: 2153,
+};
+const keys = Object.keys(apartment);
+
+const values = Object.values(apartment);
+
+// =============== 16 ===============!!!!!
+
+function countTotalSalary(salaries) {
+  let totalSalary = 0;
+
+  const values = Object.values(salaries);
+
+  for (const value of values) {
+    totalSalary += value;
+  }
+  return totalSalary;
+}
+
+// =============== 17 ===============!!!!!
+
+const colors = [
+  { hex: '#f44336', rgb: '244,67,54' },
+  { hex: '#2196f3', rgb: '33,150,243' },
+  { hex: '#4caf50', rgb: '76,175,80' },
+  { hex: '#ffeb3b', rgb: '255,235,59' },
+];
+
+const hexColors = [];
+const rgbColors = [];
+
+for (const color of colors) {
+  hexColors.push(color.hex);
+  rgbColors.push(color.rgb);
+}
+
+// =============== 18 ===============!!!!!
+
+const products = [
+  { name: 'Radar', price: 1300, quantity: 4 },
+  { name: 'Scanner', price: 2700, quantity: 3 },
+  { name: 'Droid', price: 400, quantity: 7 },
+  { name: 'Grip', price: 1200, quantity: 9 },
+];
+
+function getProductPrice(productName) {
+  for (const product of products) {
+    let name = product.name;
+    if (name === productName) {
+      return product.price;
+    }
+  }
+  return null;
+}
+
+// =============== 19 ===============!!!!!
+
+const products = [
+  { name: 'Radar', price: 1300, quantity: 4 },
+  { name: 'Scanner', price: 2700, quantity: 3 },
+  { name: 'Droid', price: 400, quantity: 7 },
+  { name: 'Grip', price: 1200, quantity: 9 },
+];
+
+function getAllPropValues(propName) {
+  const array = [];
+  for (const product of products) {
+    if (product[propName]) {
+      array.push(product[propName]);
+    }
+  }
+  return array;
+}
+
+// =============== 20 ===============!!!!!
+
+const products = [
+  { name: 'Radar', price: 1300, quantity: 4 },
+  { name: 'Scanner', price: 2700, quantity: 3 },
+  { name: 'Droid', price: 400, quantity: 7 },
+  { name: 'Grip', price: 1200, quantity: 9 },
+];
+function calculateTotalPrice(productName) {
+  let total = 0;
+  for (const prop of products) {
+    let name = prop.name;
+    if (productName === name) {
+      total = prop.price * prop.quantity;
+    }
+  }
+  return total;
+}
+
+// =============== 21 ===============
+
+const highTemperatures = {
+  yesterday: 28,
+  today: 26,
+  tomorrow: 33,
+};
+
+const { yesterday, today, tomorrow } = highTemperatures;
+
+const meanTemperature = (yesterday + today + tomorrow) / 3;
+
+// =============== 22 ===============
+
+const highTemperatures = {
+  yesterday: 28,
+  today: 26,
+  tomorrow: 33,
+};
+
+const {
+  yesterday,
+  today,
+  tomorrow,
+  icon = 'https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg',
+} = highTemperatures;
+
+const meanTemperature = (yesterday + today + tomorrow) / 3;
+
+// =============== 23 ===============
+
+const highTemperatures = {
+  yesterday: 28,
+  today: 26,
+  tomorrow: 33,
+};
+
+const {
+  yesterday: highYesterday,
+  today: highToday,
+  tomorrow: highTomorrow,
+  icon: highIcon = 'https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg',
+} = highTemperatures;
+
+const meanTemperature = (highYesterday + highToday + highTomorrow) / 3;
+
+// =============== 24 =============== (!)
+
+const colors = [
+  { hex: '#f44336', rgb: '244,67,54' },
+  { hex: '#2196f3', rgb: '33,150,243' },
+  { hex: '#4caf50', rgb: '76,175,80' },
+  { hex: '#ffeb3b', rgb: '255,235,59' },
+];
+
+const hexColors = [];
+const rgbColors = [];
+
+for (const { hex, rgb } of colors) {
+  hexColors.push(hex);
+  rgbColors.push(rgb);
+}
+
+// =============== 25 ===============
+
+const forecast = {
+  today: {
+    low: 28,
+    high: 32,
+    icon: 'https://www.flaticon.com/svg/static/icons/svg/861/861059.svg',
+  },
+  tomorrow: {
+    low: 27,
+    high: 31,
+  },
+};
+// Change code below this line
+const {
+  today: {
+    high: highToday,
+    low: lowToday,
+    icon: todayIcon = 'https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg',
+  },
+  tomorrow: {
+    high: highTomorrow,
+    low: lowTomorrow,
+    icon: tomorrowIcon = 'https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg',
+  },
+} = forecast;
+
+// =============== 26 ===============
+
+function calculateMeanTemperature(forecast) {
+  const {
+    today: { low: todayLow, high: todayHigh },
+    tomorrow: { low: tomorrowLow, high: tomorrowHigh },
+  } = forecast;
+
+  return (todayLow + todayHigh + tomorrowLow + tomorrowHigh) / 4;
+}
+
+// =============== 27 ===============
