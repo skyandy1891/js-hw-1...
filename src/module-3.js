d17885887a3add5b02285888532434248164cb31
skyandy1891 <skyandy1891@gmail.com>
2021-05-30 11:21:56 +0300

    module-3 js done

--- src/module-3.js
+++ src/module-3.js
@@ -407,3 +407,213 @@ function calculateMeanTemperature(forecast) {
 }
 
 // =============== 27 ===============
+
+const scores = [89, 64, 42, 17, 93, 51, 26];
+// Change code below this line
+const bestScore = Math.max(...scores);
+const worstScore = Math.min(...scores);
+
+// =============== 28 ===============
+
+const firstGroupScores = [64, 42, 93];
+const secondGroupScores = [89, 14, 51, 26];
+const thirdGroupScores = [29, 47, 18, 97, 81];
+// Change code below this line
+const allScores = [
+  ...firstGroupScores,
+  ...secondGroupScores,
+  ...thirdGroupScores,
+];
+const bestScore = Math.max(...allScores);
+const worstScore = Math.min(...allScores);
+
+// =============== 29 ===============
+
+const defaultSettings = {
+  theme: 'light',
+  public: true,
+  withPassword: false,
+  minNumberOfQuestions: 10,
+  timePerQuestion: 60,
+};
+const overrideSettings = {
+  public: false,
+  withPassword: true,
+  timePerQuestion: 30,
+};
+// Change code below this line
+const finalSettings = { ...defaultSettings, ...overrideSettings };
+
+// =============== 30 ===============!!!
+
+function makeTask(data) {
+  const completed = false;
+  const category = 'General';
+  const priority = 'Normal';
+  // Change code below this line
+  const newData = { completed, category, priority, ...data };
+  // Change code above this line
+  return newData;
+}
+
+// =============== 31 ===============!!!
+
+function add(...args) {
+  let value = 0;
+  for (arg of args) {
+    value += arg;
+  }
+  return value;
+}
+
+// =============== 32 ===============!!!
+
+function addOverNum(values, ...args) {
+  let total = 0;
+  for (const arg of args) {
+    if (arg > values) {
+      total += arg;
+    }
+  }
+  return total;
+}
+
+// =============== 33 ===============!!!
+
+function findMatches(...args) {
+  const matches = []; // Don't change this line
+  for (let i = 1; i < args.length; i += 1) {
+    if (args[0].includes(args[i])) matches.push(args[i]);
+  }
+  return matches;
+}
+
+// =============== 34 ===============!!!
+
+const bookShelf = {
+  books: ['The last kingdom', 'The guardian of dreams'],
+  getBooks() {
+    return 'Returning all books';
+  },
+  addBook(bookName) {
+    return `Adding book ${bookName}`;
+  },
+  removeBook(bookName) {
+    return `Deleting book ${bookName}`;
+  },
+  updateBook(oldName, newName) {
+    return `Updating book ${oldName} to ${newName}`;
+  },
+};
+
+// =============== 35 ===============!!!
+
+const bookShelf = {
+  books: ['The last kingdom', 'Haze', 'The guardian of dreams'],
+  updateBook(oldName, newName) {
+    // Change code below this line
+
+    const index = this.books.indexOf(oldName);
+    this.books.splice(index, 1, newName);
+
+    // Change code above this line
+  },
+};
+
+// =============== 36 ===============
+
+const atTheOldToad = {
+  potions: [],
+};
+
+// =============== 37 ===============
+
+const atTheOldToad = {
+  potions: ['Speed potion', 'Dragon breath', 'Stone skin'],
+  getPotions() {
+    return this.potions;
+  },
+};
+
+// =============== 38 ===============!!!
+
+const atTheOldToad = {
+  potions: ['Speed potion', 'Dragon breath', 'Stone skin'],
+  addPotion(potionName) {
+    // Change code below this line
+    this.potions.push(potionName);
+    return this.potions;
+  },
+};
+
+// =============== 39 ===============!!!
+
+const atTheOldToad = {
+  potions: ['Speed potion', 'Dragon breath', 'Stone skin'],
+  removePotion(potionName) {
+    // Change code below this line
+    this.potions.splice(this.potions.indexOf(potionName), 1);
+    // Change code above this line
+  },
+};
+
+// =============== 40 ===============!!!
+
+const atTheOldToad = {
+  potions: ['Speed potion', 'Dragon breath', 'Stone skin'],
+  updatePotionName(oldName, newName) {
+    // Change code below this line
+
+    const index = this.potions.indexOf(oldName);
+    this.potions.splice(index, 1, newName);
+
+    // Change code above this line
+  },
+};
+
+// =============== 41 ===============!!!
+
+const atTheOldToad = {
+  potions: [
+    { name: 'Speed potion', price: 460 },
+    { name: 'Dragon breath', price: 780 },
+    { name: 'Stone skin', price: 520 },
+  ],
+  // Change code below this line
+  getPotions() {
+    return this.potions;
+  },
+  addPotion(potionName) {
+    const { potions } = this;
+    const { name } = potionName;
+    for (let i = 0; i < potions.length; i += 1) {
+      if (potions[i].name === name) {
+        return `Potion ${name} is already equipped!`;
+      }
+      potions.push(potionName);
+    }
+  },
+  removePotion(potionName) {
+    const { potions } = this;
+    for (let i = 0; i < potions.length; i += 1) {
+      if (potions[i].name === potionName) {
+        potions.splice(i, 1);
+        return potions;
+      }
+    }
+    return `Potion ${potionName} is not in inventory!`;
+  },
+  updatePotionName(oldName, newName) {
+    const { potions } = this;
+    for (let i = 0; i < potions.length; i += 1) {
+      if (potions[i].name === oldName) {
+        potions[i].name = newName;
+        return potions;
+      }
+    }
+    return `Potion ${oldName} is not in inventory!`;
+  },
+  // Change code above this line
+};
+
+// ==================================== DONE !!! ====================================
