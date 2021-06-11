f84efe6512bd59ed032cc8350ca3b0356213c8c8
skyandy1891 <skyandy1891@gmail.com>
2021-06-11 18:39:42 +0300

    module-5.js done

--- src/module-5.js
+++ src/module-5.js
@@ -325,11 +325,11 @@ class User {
 
 class Admin extends User {
   // Пиши код ниже этой строки
-  accessLevel;
   constructor({ email, accessLevel }) {
     super(email);
     this.accessLevel = accessLevel;
   }
+  accessLevel;
 
   static AccessLevel = {
     BASIC: 'basic',
@@ -339,4 +339,55 @@ class Admin extends User {
   // Пиши код выше этой строки
 }
 
-// ==================== 19 ====================
+// ==================== 19 ====================!!!
+
+class User {
+  email;
+  constructor(email) {
+    this.email = email;
+  }
+  get email() {
+    return this.email;
+  }
+  set email(newEmail) {
+    this.email = newEmail;
+  }
+}
+class Admin extends User {
+  // Пиши код ниже этой строки
+  static AccessLevel = {
+    BASIC: 'basic',
+    SUPERUSER: 'superuser',
+  };
+  accessLevel;
+  blacklistedEmails;
+  constructor({ email, accessLevel, blacklistedEmails = [] }) {
+    super(email);
+    this.accessLevel = accessLevel;
+    this.blacklistedEmails = blacklistedEmails;
+  }
+  blacklist(email) {
+    this.blacklistedEmails.push(email);
+  }
+  isBlacklisted(email) {
+    if (this.blacklistedEmails.includes(email)) {
+      return true;
+    }
+    return false;
+  }
+  // Пиши код выше этой строки
+}
+
+const mango = new Admin({
+  email: 'mango@mail.com',
+  accessLevel: Admin.AccessLevel.SUPERUSER,
+});
+
+console.log(mango.email); // mango@mail.com
+console.log(mango.accessLevel); // superuser
+mango.blacklist('poly@mail.com');
+console.log(mango.blacklistedEmails); // 'poly@mail.com'
+console.log(mango.isBlacklisted('mango@mail.com')); //  false
+console.log(mango.isBlacklisted('poly@mail.com')); // true
+
+// ==================== DONE ====================
