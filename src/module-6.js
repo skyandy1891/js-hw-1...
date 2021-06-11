bc42b75ce24ede1dc3362b2ec684ae9eca1731cb
skyandy1891 <skyandy1891@gmail.com>
2021-06-11 19:16:46 +0300

    task 1 added

---
+++ src/module-6.js
@@ -0,0 +1,15 @@
+// ================= 1 ================
+
+function calculateTotalPrice(orderedItems) {
+  let totalPrice = 0;
+  // Пиши код ниже этой строки
+
+  for (let i = 0; i < orderedItems.length; i += 1) {
+    totalPrice += orderedItems[i];
+  }
+  orderedItems.forEach(function (orderedItems) {});
+  // Пиши код выше этой строки
+  return totalPrice;
+}
+
+// ================= 2 ================
