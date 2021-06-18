2550e1929377df31f6a4045e4b5731d66e6d6fbe
skyandy1891 <skyandy1891@gmail.com>
2021-06-19 00:02:52 +0300

    task 5 didn't finish

--- src/module-6.js
+++ src/module-6.js
@@ -17,15 +17,48 @@ orderedItems.forEach(element => (totalPrice += element));
 // ================= 2 ================!!!
 
 function filterArray(numbers, value) {
-    const filteredNumbers = [];
-    // Пиши код ниже этой строки
-     numbers.forEach( number => {
-       if (number > value) {
-         filteredNumbers.push(number);
-       }
-     });
-    // Пиши код выше этой строки
-    return filteredNumbers;
-  }
+  const filteredNumbers = [];
+  // Пиши код ниже этой строки
+  numbers.forEach(number => {
+    if (number > value) {
+      filteredNumbers.push(number);
+    }
+  });
+  // Пиши код выше этой строки
+  return filteredNumbers;
+}
 
 // ================= 3 ================!!!
+
+function getCommonElements(firstArray, secondArray) {
+  const commonElements = [];
+  // Пиши код ниже этой строки
+  firstArray.forEach(elem =>
+    secondArray.includes(elem) ? commonElements.push(elem) : elem,
+  );
+  return commonElements;
+}
+
+// ================= 4 ================
+
+// Пиши код ниже этой строки
+
+const calculateTotalPrice = (quantity, pricePerItem) => {
+  // Пиши код выше этой строки
+
+  return quantity * pricePerItem;
+};
+
+// ================= 5 ================ доделать!!!!!
+
+// Пиши код ниже этой строки
+function calculateTotalPrice(orderedItems) {
+  let totalPrice = 0;
+
+  orderedItems.forEach(item => {
+    totalPrice += item;
+  });
+
+  return totalPrice;
+}
+// Пиши код выше этой строки
