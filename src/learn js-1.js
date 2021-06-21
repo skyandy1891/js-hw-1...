34d112110ffdd3f2fafc784c32503f3bdb529ba6
skyandy1891 <skyandy1891@gmail.com>
2021-06-21 11:13:00 +0300

    learn js-1 added

---
+++ src/learn js-1.js
@@ -0,0 +1,19 @@
+// const shouldRenew = confirm('продлить подписку?');
+// console.log(shouldRenew);
+
+// const shouldNew = confirm('вы согласны отдать нам все ваши деньги?');
+// console.log(shouldRenew);
+
+// const sure = confirm('точно хотите?');
+// console.log(shouldRenew);
+
+// let quantity = prompt('введите номер карты');
+// quantity = Number(quantity);
+// console.log(quantity);
+// // console.log(typeof quantity);
+
+// const renew = confirm('спасибо,вы лошара!');
+// console.log(shouldRenew);
+
+// let Feedback = prompt('оставьте пожалуйста отзыв о нашей компании)))');
+// console.log(Feedback);
